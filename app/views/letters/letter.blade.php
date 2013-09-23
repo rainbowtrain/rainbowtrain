36c77509026fae6c54b16ca3803dfa0dc75693dd
root <matthew.day@hyfn.com>
2013-09-23 00:09:10 -0700

    made actual animation using an image.. and a clever hack

--- app/views/letters/letter.blade.php
+++ app/views/letters/letter.blade.php
@@ -1,14 +1,68 @@
-@extends('layouts.layout')
+<!DOCTYPE HTML>
+<html>
+	<head>
+    	<style>
+      		body {
+        		margin: 0px;
+        		padding: 0px;
+      		}
+    	</style>
+  	</head>
+  	<body>
+    	<canvas id="letter_canvas"></canvas>
+    	<script>
+	  		
+			// set canvas to full screen
+			c = document.getElementById('letter_canvas');
+			c.width = document.body.clientWidth;
+    		c.height = document.body.clientHeight;
+      		
+			window.requestAnimFrame = (function(callback) {
+        		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+        		function(callback) {
+          			window.setTimeout(callback, 1000 / 60);
+        		};
+      		})();
 
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
- 	<div class="container">
-   
-	<!-- Example row of columns -->
-  	<div class="row">
-		<div class="col-lg-4">
-	  	<h2>You selected letter {{$letter}}</h2>
-	</div>
-	
-</div>
+		  	function drawImg(img, context) {
+				context.drawImage(img, img.alt, img.y);
+			}
+			function animate(img, canvas, context, startTime) {
+				// update
+				var time = (new Date()).getTime() - startTime;
 
+				var linearSpeed = 100;
+				// pixels / second
+				var newX = linearSpeed * time / 1000;
+				if(newX < canvas.width - img.width / 2) {
+					img.alt = newX;
+					// here's a fucking hack... and a 1/2 since img object doesn't have x and y, they do have alt and name... bwahahaha
+				}
+
+				// clear
+				context.clearRect(0, 0, canvas.width, canvas.height);
+
+				drawImg(img, context);
+
+				// request new frame
+				requestAnimFrame(function() {
+					animate(img, canvas, context, startTime);
+				});
+			}
+			
+			var canvas = document.getElementById('letter_canvas');
+			var context = canvas.getContext('2d');
+
+			var img = new Image();
+			img.src = '/assets/img/airplane.png';
+
+      		drawImg(img, context);
+
+      		// wait one second before starting animation
+      		setTimeout(function() {
+        		var startTime = (new Date()).getTime();
+        		animate(img, canvas, context, startTime);
+      		}, 1000);
+    	</script>
+  	</body>
+</html>
