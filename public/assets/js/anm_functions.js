d71db8f9ddd841e9bfe17ed4b5a0e6c47fefed18
Matthew Day <matthew.scott.day@gmail.com>
2013-10-03 15:29:58 -0700

    zig zag

--- public/assets/js/anm_functions.js
+++ public/assets/js/anm_functions.js
@@ -1,8 +1,10 @@
+// need some fucking global variables
+
 setTimeout(function() {
 	var startTime = (new Date()).getTime();
-    lefttoright(img, canvas, context, startTime);
+    zigzag(img, canvas, context, startTime, 0, 200, 300, 100);
 }, 600);
-			
+
 window.requestAnimFrame = (function(callback) {
 	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
 	function(callback) {
@@ -17,7 +19,6 @@ function lefttoright(img, canvas, context, startTime) {
 	var newX = 100 * time / 500;
 	if(newX < canvas.width - (img.width - 250) / 2) {
 		img.alt = newX;
-		// here's a fucking hack... and a 1/2 since img object doesn't have x and y, they do have alt and name... bwahahaha
 	}
 
 	// clear
@@ -28,5 +29,54 @@ function lefttoright(img, canvas, context, startTime) {
 	// request new frame
 	requestAnimFrame(function() {
 		lefttoright(img, canvas, context, startTime);
+	});
+}
+
+function zigzag(img, canvas, context, startTime, startx, starty, bottom_threshold, top_threshold) {
+
+    var time = (new Date().getTime() - startTime);
+   
+    // figure out rate of x to move left to right 
+    var newX = 100 * time / 500;
+	if(newX < canvas.width - (img.width - 250) / 2) {
+		img.alt = newX;
+	}
+  
+    // FUCK tHIS GODDAMN LOGIC SUCKS 
+    // if y is at a certain point below "bottom_threshold" then move y axis back toward 0 
+    if (img.name >= bottom_threshold) {
+        img.name = (parseInt(img.name) - 2); // go up
+    } else { // move down
+        img.name = (parseInt(img.name) + 2); // go down
+    }
+    // and the inverse of above if statement logic
+    if (img.name >= top_threshold) {
+        img.name = (parseInt(img.name) + 2); // go up
+    } else { // move down
+        img.name = (parseInt(img.name) - 2); // go down
+    }
+  
+    // and the other way!
+    if (img.name <= bottom_threshold) {
+        img.name = (parseInt(img.name) + 2); // go up
+    } else { // move down
+        img.name = (parseInt(img.name) - 2); // go down
+    }
+    // and the inverse of above if statement logic
+    if (img.name <= top_threshold) {
+        img.name = (parseInt(img.name) - 2); // go up
+    } else { // move down
+        img.name = (parseInt(img.name) + 2); // go down
+    }
+
+ 
+	// clear
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	context.drawImage(img, img.alt, img.name);
+
+	// request new frame
+	requestAnimFrame(function() {
+		zigzag(img, canvas, context, startTime, img.alt, img.name, bottom_threshold, top_threshold);
 	});
 }
